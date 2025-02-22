7e4eb1e0f269b7f23d85ec3aa91c17ad7a9400d6
satyendra kumar <satyendra1es1ds@gmail.com>
2025-02-23 00:05:36 +0530

    port changed

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get("*", (req, res)=>{
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
 })
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port , ()=>{
     console.log(`Server running at port ${port}`)
 })
