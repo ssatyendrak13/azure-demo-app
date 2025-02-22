899fbd14dd683b4a8c24484163aeaf52ac4bfc80
satyendra kumar <satyendra1es1ds@gmail.com>
2025-02-23 00:02:07 +0530

    added port

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get("*", (req, res)=>{
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
 })
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 app.listen(port , ()=>{
     console.log(`Server running at port ${port}`)
 })
