a47658efc79604960ea136b0f5a0bd4ef7ca93b3
satyendra kumar <satyendra1es1ds@gmail.com>
2025-02-22 23:39:04 +0530

    build static file

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from 'path';
 
 const app = express();
 dotenv.config();
 
-app.get('/', (req, res)=>{
-    res.send("Hello from backend")
-})
+// app.get('/', (req, res)=>{
+//     res.send("Hello from backend")
+// })
 
-app.get('/login', (req, res)=>{
-    res.send("This is login page");
+// app.get('/login', (req, res)=>{
+//     res.send("This is login page");
+// })
+
+// production script 
+app.use(express.static("./frontend/build"))
+app.get("*", (req, res)=>{
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
 })
 
 const port = process.env.PORT;
